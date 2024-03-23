18643219ae78f70257e37f453c51428ec6e206d1
RmVa23 <148461819+RmVa23@users.noreply.github.com>
2024-03-23 15:17:04 +0000

    eda graficos

--- src/app.py
+++ src/app.py
@@ -1,4 +1,12 @@
+import pandas as pd
 from utils import db_connect
+
 engine = db_connect()
 
-# your code here
+
+engine
+if engine:
+    print("Conexión exitosa")
+
+data = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/decision-tree-project-tutorial/main/diabetes.csv')
+data.to_sql('tabla diabetes', engine, if_exists='replace', index=False)
